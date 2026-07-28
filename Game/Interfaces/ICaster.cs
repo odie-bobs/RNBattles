fed38d4fcf99fddcfa3381b9792c18f9f51c443c
odie-bobs <140290205+odie-bobs@users.noreply.github.com>
2026-07-28 15:48:17 -0500

    feat: add fireball
    updated to factory method, attempting to implement abstract factory

--- Game/Interfaces/ICaster.cs
+++ Game/Interfaces/ICaster.cs
@@ -16,12 +16,17 @@ public interface ICaster : ICharacter
 
     IHit ICharacter.CreateHit()
     {
-        if (!IsHit()) return new Hit() { IsHit = false };
+        if (!IsHit())
+            return new Fireball() { IsHit = false };
 
         var isCriticalHit = IsCriticalHit();
         var baseDamage = Random.Next(Damage.min, Damage.max);
         var damage = isCriticalHit ? baseDamage * 2 : baseDamage;
-        return new Hit { IsHit = true, IsCritical = isCriticalHit, Damage = damage };
-
+        return new Fireball
+        {
+            IsHit = true,
+            IsCritical = isCriticalHit,
+            Damage = damage,
+        };
     }
 }
