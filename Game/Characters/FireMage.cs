ef065cd44b9aa5e083b31442514d05f567d93f20
odie-bobs <140290205+odie-bobs@users.noreply.github.com>
2026-07-28 16:42:36 -0500

    fix: update Mage -> FireMage

--- Game/Characters/FireMage.cs
+++ Game/Characters/FireMage.cs
@@ -1,11 +1,14 @@
+using Game.Attacks;
 using Game.Interfaces;
 
 namespace Game.Characters;
 
-public class Mage(int min, int max) : ICaster
+public class FireMage(int min, int max) : ICaster
 {
     public Random Random { get; init; } = new();
     public int HitPoints { get; set; } = 100;
     public bool IsAlive { get; set; } = true;
     public (int min, int max) Damage { get; set; } = (min, max);
+
+    IHit ICaster.CreateSpell() => new Fireball();
 }
